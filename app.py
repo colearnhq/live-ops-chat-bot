79fa9135a676cb5bf26f662a455f21b509105770
data-live-ops <imam.fachrudin@colearn.id>
2024-07-15 21:34:42 +0700

    fix: change .env path

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from slack_sdk.errors import SlackApiError
 from database import SheetManager
 import pytz
 
-load_dotenv(".env.dev")
+load_dotenv(".env")
 
 creds_dict = {
     "type": os.getenv("GOOGLE_CREDENTIALS_TYPE"),
