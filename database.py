f3b9b6274f80044137f724c58043273d9a780554
data-live-ops <imam.fachrudin@colearn.id>
2024-09-26 12:09:26 +0700

    removed: the log

--- database.py
+++ database.py
@@ -148,7 +148,6 @@ class SheetManager:
         }
 
     def update_piket(self, piket_id, updates):
-        print(f"check piket id: {piket_id}")
         try:
             row = self.find_piket_row(piket_id)
             if row:
@@ -157,7 +156,6 @@ class SheetManager:
                     if "at" in key and isinstance(value, datetime):
                         value = self.convert_to_local_time(value)
                     self.piket_sheet.update_cell(row, col, value)
-                    print(f"row ke {row}, col ke {col}, dengan value {value}")
         except Exception as e:
             logging.error(f"Failed to update ticket: {str(e)}")
 
