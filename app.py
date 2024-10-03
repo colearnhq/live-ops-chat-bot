dfe98b6532b6d4b8bf3dc7e95f8232e49e1abfd3
data-live-ops <imam.fachrudin@colearn.id>
2024-10-03 07:13:32 +0700

    fix: mispath of selected cat

--- app.py
+++ app.py
@@ -761,9 +761,11 @@ def handle_category_selection(ack, body, client):
 @app.action("generate_slot_list")
 def handle_generate_slot_list(ack, body, client):
     ack()
-    [channel_id, selected_cat_on_piket] = body["view"]["private_metadata"].split("@@")
-    print(f"cek {selected_cat_on_piket}")
-    grade = body["view"]["state"]["values"]["grade_block"]["grade_action"]["value"]
+    state = body["view"]["state"]["values"]
+    selected_cat_on_piket = state["teacher_replace_block"]["teacher_replace_action"][
+        "value"
+    ]
+    grade = state["grade_block"]["grade_action"]["value"]
     slots = sheet_manager.get_slots_by_grade(grade)
     dropdown_options = [
         {"text": {"type": "plain_text", "text": slot}, "value": slot} for slot in slots
