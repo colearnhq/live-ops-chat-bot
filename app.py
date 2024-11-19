579a1d79953cebde714927b29fe1729fea150478
data-live-ops <imam.fachrudin@colearn.id>
2024-11-19 07:11:27 +0700

    log: cek some logs

--- app.py
+++ app.py
@@ -390,7 +390,7 @@ def handle_category_selection(ack, body, client):
         else body["actions"][0]["value"]
     )
     trigger_id = body["trigger_id"]
-    print(f"cek category: {body['view']['state']['values']}")
+    print(f"cek category: {selected_category}")
     if user_input == "Piket":
         modal_blocks = [
             {
